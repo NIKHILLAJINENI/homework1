f7185f8e37d41491778deee0c794163fc551338b
NIKHILLAJINENI <nickilllajineni@gmail.com>
2021-09-08 13:25:01 +0530

    cp1

---
+++ Ticket.java
@@ -0,0 +1,25 @@
+package com.upgrade.flightreservationsystem;
+//just for time being to complete checkpoint 1.later will change the code as the project checkpoints moves further.
+public class Ticket {
+public String pnr;
+public String from;
+public String to;
+public Flight flight;
+public String departureDateTime;
+public String arrivalDatetime;
+public Passenger passenger;
+public String seatno;
+public float price;
+public boolean cancelled;
+
+
+public String checkStatus() {
+	return null;
+}
+public int getFlightDuration() {
+	return  0;
+}
+public void cancel() {
+
+}
+}
